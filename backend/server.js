416b49a4f35d0e2ac58b5f0509e585e6371028a7
HajiraImran <hajiraimran900@gmail.com>
2026-01-08 07:09:02 -0800

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -44,10 +44,10 @@ app.get("/api", (req, res) => {
 });
 
 /* ===== Serve React Frontend ===== */
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 /* ===== MongoDB Connection ===== */
